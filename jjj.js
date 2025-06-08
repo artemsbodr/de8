1a75629a07595662b151ce339f3ea6fd259dab17
MrArtem <artembodrik210311@gmail.com>
2025-06-08 22:55:10 +0300

    first commit

---
+++ jjj.js
@@ -0,0 +1,17 @@
+//1
+
+const al = ['1', '2', '3', '4', '5'];
+console.log(al.join(','));
+
+//2
+
+const cart = ['1', '2', '3', '4', '5'];
+
+cart.splice(2)
+console.log(cart.indexOf('3'))
+console.log(cart)
+
+cart.unshift('-1','-2')
+console.log(cart)
+
+// console.log(cart.includes())
