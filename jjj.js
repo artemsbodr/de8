adad28b60c8f2cf4cdf081939b8ebd81ad27fa9a
MrArtem <artembodrik210311@gmail.com>
2025-06-16 18:35:39 +0300

    pizza_push

--- jjj.js
+++ jjj.js
@@ -1,17 +1,26 @@
 //1
 
-const al = ['1', '2', '3', '4', '5'];
-console.log(al.join(','));
+const al = ["1", "2", "3", "4", "5"];
+console.log(al.join(","));
 
 //2
 
-const cart = ['1', '2', '3', '4', '5'];
+const cart = [
+  "Карточка-1",
+  "Карточка-2",
+  "Карточка-3",
+  "Карточка-4",
+  "Карточка-5",
+];
 
-cart.splice(2)
-console.log(cart.indexOf('3'))
-console.log(cart)
+console.log(cart);
+cart.splice("2");
+console.log(cart.indexOf("Карточка-3"));
+cart.push("Карточка-4");
+console.log(cart);
+cart.push("Карточка-5(!Нова!)");
 
-cart.unshift('-1','-2')
-console.log(cart)
+cart.unshift("Карточка-0", "Карточка-00");
+console.log(cart);
 
 // console.log(cart.includes()).=
